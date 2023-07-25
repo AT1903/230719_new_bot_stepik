148bf8fc6c68960fe057f94f7bfd1cfc3ba808ba
Tihonov Aleksandr <tihonov1903@gmail.com>
2023-07-25 16:22:47 +0500

    Работа с Requests

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-print('hello')
-print('new code')
+import requests
+
+print('-----Requests lib-----')
 #файл заполнен в VScode перед отправкой в репозиторий GitHub
+
+api_url = 'http://api.open-notify.org/iss-now.json'
+response = requests.get(api_url)  # Отправляем GET-запрос и сохраняем ответ в переменной response
+
+if response.status_code == 200:
+    print(response.text)
+else:
+    print(f'Код статуса - {response.status_code}')
+
+
+print ('----API for numbers----')
+
+api_url = 'http://numbersapi.com/43'
+response = requests.get(api_url)  # Отправляем GET-запрос и сохраняем ответ в переменной response
+
+if response.status_code == 200:
+    print(response.text)
+else:
+    print(f'Код статуса - {response.status_code}')
